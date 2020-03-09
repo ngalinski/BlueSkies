f1d525235f658a26f1c34872ebca377b3d8d4696
Nicholas Galinski <galinski.n@husky.neu.edu>
2020-03-08 23:38:58 -0400

    added drug table

---
+++ sql_tables/drug.sql
@@ -0,0 +1,32 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Drug`
+--
+
+DROP TABLE IF EXISTS `Drug`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+
+CREATE TABLE `Drug` (
+  `DrugCode` int NOT NULL AUTO_INCREMENT,
+  `DrugID` varchar(25) NOT NULL,
+  `CompanyName` varchar(255) NOT NULL,
+  `DrugName` varchar(255) NOT NULL,
+  `Strength` varchar(45) NOT NULL,
+  `Route` varchar(45) NOT NULL,
+  PRIMARY KEY (`DrugCode`),
+  KEY `fk_DrugUtilizationDrugCode` (`DrugCode`),
+  CONSTRAINT `fk_DrugCompanyName` FOREIGN KEY (`CompanyName`) REFERENCES `PharmaCompanies` (`CompanyName`),
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
