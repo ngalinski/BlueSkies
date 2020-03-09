950c07d30c18f1e8a2ea7a17a93556cc79531472
Amy Stefani <amystefani1@gmail.com>
2020-03-08 23:44:31 -0700

    updating question3 comment

--- pm3_sql_select_statements/question3.sql
+++ pm3_sql_select_statements/question3.sql
@@ -1,4 +1,4 @@
--- 3. What are the top 5 counties in the U.S. rated by air quality? What is the top county in each state? 
+-- 3. What are the top 5 counties in the U.S. rated by air quality?
 	SELECT AirQuality.CountyCode, County.CountyName, AirQuality.StateCode, AirQuality.MedianAQI FROM AirQuality
     LEFT JOIN County ON County.CountyCode = AirQuality.CountyCode
 		ORDER BY AirQuality.MedianAQI DESC
