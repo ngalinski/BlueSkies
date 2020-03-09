7176e7e0cb025de4d6fcc38e55215757f6ffe674
Amy Stefani <amystefani1@gmail.com>
2020-03-08 23:26:15 -0700

    qs 9 and 10

--- pm3_sql_select_statements/question10.sql
+++ pm3_sql_select_statements/question10.sql
@@ -1,5 +1,15 @@
-SELECT CompanyName, COUNT(*)
-FROM Drug
-GROUP BY CompanyName
-ORDER BY CompanyName DESC
-LIMIT 10;
+-- 10. What are the top 5 states with the least hospitals per 1000 residents?
+
+SELECT t2.StateCode, t1.NumHospitals / (t2.TotalPop / 10000) as HospitalPer10000Residents, t1.NumHospitals, t2.TotalPop FROM
+	(SELECT Count(t0.HospitalCode) as NumHospitals, t0.StateCode FROM
+	(SELECT Hospital.HospitalCode, Hospital.ZipCode, Location.StateCode 
+		FROM Hospital JOIN Location ON Hospital.ZipCode = Location.ZipCode 
+		ORDER BY Location.StateCode ASC) t0
+		GROUP BY t0.StateCode) t1
+JOIN (
+	SELECT Sum(Population) as TotalPop, StateCode FROM Location
+	   GROUP BY StateCode
+	   ORDER BY StateCode ASC ) t2
+	ON t2.StateCode = t1.StateCode
+ORDER BY HospitalPer10000Residents ASC
+LIMIT 5;
--- pm3_sql_select_statements/question9.sql
+++ pm3_sql_select_statements/question9.sql
@@ -1,3 +1,15 @@
-SELECT StateCode, DrugName, MAX(NumRX)
-FROM DrugUtilization
-GROUP BY StateCode;
+-- 9. What are the top 5 states with the most hospitals per 1000 residents?
+
+SELECT t2.StateCode, t1.NumHospitals / (t2.TotalPop / 10000) as HospitalPer10000Residents, t1.NumHospitals, t2.TotalPop FROM
+	(SELECT Count(t0.HospitalCode) as NumHospitals, t0.StateCode FROM
+	(SELECT Hospital.HospitalCode, Hospital.ZipCode, Location.StateCode 
+		FROM Hospital JOIN Location ON Hospital.ZipCode = Location.ZipCode 
+		ORDER BY Location.StateCode ASC) t0
+		GROUP BY t0.StateCode) t1
+JOIN (
+	SELECT Sum(Population) as TotalPop, StateCode FROM Location
+	   GROUP BY StateCode
+	   ORDER BY StateCode ASC ) t2
+	ON t2.StateCode = t1.StateCode
+ORDER BY HospitalPer10000Residents DESC
+LIMIT 5;
