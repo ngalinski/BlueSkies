f88cd1cf130ff8c5c9088d3e413a87040aca9541
Nicholas Galinski <galinski.n@husky.neu.edu>
2020-03-21 14:08:14 -0700

    update aq file

--- pm4 /model/AirQuality.java
+++ pm4 /model/AirQuality.java
@@ -1,1 +1,13 @@
 package model;
+
+public class AirQuality {
+  private int DaysWithAI;
+  private int GoodDaysCount;
+  private int ModerateDaysCount;
+  private int UnhealthyForSensitiveDaysCount;
+  private int UnleathyDays;
+  private int VeryUnhealthyDays;
+  private int HazardousDays;
+  private int 90thPercentileAQI;
+  private int AirQualityCode;
+}
