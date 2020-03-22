90aca2fa3dcdf05b7c47560cdd084ba4c88fc6ed
Nicholas Galinski <galinski.n@husky.neu.edu>
2020-03-22 13:04:58 -0700

    updated hcu dao

--- pm4 /dal/HealthCareUtilizationDAO.java
+++ pm4 /dal/HealthCareUtilizationDAO.java
@@ -9,6 +9,47 @@ import java.sql.SQLException;
 
 public class HospitalCareUtilizationDAO {
   protected ConnectionManager connectionManager;
-  private static HospitalQualityDAO instance = null;
+  private static HealthCareUtilizationDAO instance = null;
 
+  protected HealthCareUtilizationDAO() {
+    connectionManager = new ConnectionManager();
+  }
+
+  public static HealthCareUtilizationDAO getInstance() {
+    if (instance == null) {
+      instance = new HospitalCareUtilizationDAO();
+    }
+    return instance;
+  }
+
+  public HealthCareUtilization create(HealthCareUtilization healthCareUtilization) throws SQLException {
+    String insertHCU = "INSERT INTO HealthCareUtilization() VALUE();";
+    Connection connection = null;
+    PreparedStatement insertStmt = null;
+    try {
+      connection = connectionManager.getConnection();
+      insertStmt = connection.prepareStatement(insertHCU);
+
+      insertStmt.executeUpdate();
+      return healthCareUtilization;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if(connection != null) {
+        connection.close();
+      }
+      if(insertStmt != null) {
+        insertStmt.close();
+      }
+    }
+  }
+
+  public HealthCareUtilization getHealthCareUtilization() throws SQLException {
+
+  }
+
+  public HealthCareUtilization delete() throws SQLException {
+
+  }
 }
