a2d6954b0d65c2126879f4db8efb41e6ce3db213
Nicholas Galinski <galinski.n@husky.neu.edu>
2020-03-22 12:57:56 -0700

    Update HealthCareUtilization.java

--- pm4 /dal/HealthCareUtilization.java
+++ pm4 /dal/HealthCareUtilization.java
@@ -1,1 +1,14 @@
 package dal;
+
+import blog.model.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class HospitalCareUtilizationDAO {
+  protected ConnectionManager connectionManager;
+  private static HospitalQualityDAO instance = null;
+
+}
