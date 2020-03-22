fb1d8c4e4b64ce00aa4a321b304c2d262ab9a753
Nicholas Galinski <galinski.n@husky.neu.edu>
2020-03-22 13:03:02 -0700

    more additions

--- pm4 /dal/HealthCareSpendingDAO.java
+++ pm4 /dal/HealthCareSpendingDAO.java
@@ -9,6 +9,47 @@ import java.sql.SQLException;
 
 public class HospitalCareSpendingDAO {
   protected ConnectionManager connectionManager;
-  private static HospitalQualityDAO instance = null;
+  private static HealthCareSpendingDAO instance = null;
 
+  protected HealthCareSpendingDAO() {
+    connectionManager = new ConnectionManager();
+  }
+
+  public static HealthCareSpendingDAO getInstance() {
+    if (instance == null) {
+      instance = new HospitalCareSpendingDAO();
+    }
+    return instance;
+  }
+
+  public HealthCareSpending create(HealthCareSpending healthCareSpending) throws SQLException {
+    String insertHCS = "INSERT INTO HealthCareSpending() VALUE();";
+    Connection connection = null;
+    PreparedStatement insertStmt = null;
+    try {
+      connection = connectionManager.getConnection();
+      insertStmt = connection.prepareStatement(insertHCS);
+
+      insertStmt.executeUpdate();
+      return healthCareSpending;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if(connection != null) {
+        connection.close();
+      }
+      if(insertStmt != null) {
+        insertStmt.close();
+      }
+    }
+  }
+
+  public HealthCareSpending getHealthCareSpending() throws SQLException {
+
+  }
+
+  public HealthCareSpening delete() throws SQLException {
+
+  }
 }
