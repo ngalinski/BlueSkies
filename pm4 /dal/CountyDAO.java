6fea79b83cc8c9cf4a8da949428c9688652f2f52
Nicholas Galinski <galinski.n@husky.neu.edu>
2020-03-22 19:35:30 -0700

    updated county dao

--- pm4 /dal/CountyDAO.java
+++ pm4 /dal/CountyDAO.java
@@ -22,15 +22,106 @@ public class CountyDAO {
     return instance;
   }
 
-  public County create() throws SQLException {
-
+  public County create(County county) throws SQLException {
+    String insertCounty = "INSERT INTO County(CountyName) VALUE(?);";
+    Connection connection = null;
+    PreparedStatement insertStmt = null;
+    try {
+      connection = connectionManager.getConnection();
+      insertStmt = connection.prepareStatement(insertCounty);
+      insertStmt.setString(1, county.getCountyName());
+      insertStmt.executeUpdate();
+      return county;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if(connection != null) {
+        connection.close();
+      }
+      if(insertStmt != null) {
+        insertStmt.close();
+      }
+    }
   }
 
-  public Count getCounty() throws SQLException {
-
+  public County getCountybyName(String countyName) throws SQLException {
+    String selectCounty = "SELECT CountyName,CountyCode,StateCode FROM Conuty WHERE countyName=?;";
+    Connection connection = null;
+    PreparedStatement selectStmt = null;
+    ResultSet results = null;
+    try {
+      connection = connectionManager.getConnection();
+      selectStmt = connection.prepareStatement(selectCounty);
+      selectStmt.setString(1, countyName);
+      results = selectStmt.executreQuery();
+      if(results.next()) {
+        String resultName = results.getString("CountyName");
+        String CountyCode = results.getString("CountyCode");
+        int StateCode = results.getString("StateCode");
+        County county = new County(resultName, CountyCode, StateCode);
+        return county;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if(connection != null) {
+        connection.close();
+      }
+      if(selectStmt != null) {
+        selectStmt.close();
+      }
+      if(results != null) {
+        results.close();
+      }
+    }
+    return null;
   }
 
-  public County delete() throws SQLException {
+  public County updateCountyName(County county, String newName) throws SQLException {
+    String updateName = "UPDATE County SET CountyName=?;";
+    Connection connection = null;
+    PreparedStatement updateStmt = null;
+    try {
+      connection = connectionManager.getConnection();
+      updateStmt = connection.prepareStatement(updateName);
+      updateStmt.setString(1, newName);
+      county.setCountyName(newName);
+      return county;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if(connection != null) {
+        connection.close();
+      }
+      if(updateStmt != null) {
+        updateStmt.close();
+      }
+    }
+  }
 
+  public County delete(County county) throws SQLException {
+    String deleteCounty = "DELETE FROM County WHERE CountyName=?;";
+    Connection connection = null;
+    PreparedStatement deleteStmt = null;
+    try {
+      connection = connectionManager.getConnection();
+      deleteStmt = connection.prepareStatement(deleteCounty);
+      deleteStmt.setString(1, county.getCountyName());
+      deleteStmt.executeUpdate();
+      return null;
+    } catch (SQLException e) {
+      e.printStackTrace();;
+      throw e;
+    } finally {
+      if (connection != null) {
+        connection.close();
+      }
+      if (deleteStmt != null) {
+        deleteStmt.close();
+      }
+    }
   }
 }
