fe99ce3dc09c5328f03ba701649fd3abf361f765
Nicholas Galinski <galinski.n@husky.neu.edu>
2020-03-22 17:22:43 -0700

    updated location dao

--- pm4 /dal/LocationDAO.java
+++ pm4 /dal/LocationDAO.java
@@ -22,15 +22,96 @@ public class LocationDAO {
     return instance;
   }
 
-  public Location create() throws SQLException {
+  public Location create(Location location) throws SQLException {
+    String insertLocation = "INSERT INTO Location(ZipCode,LocationName) VALUE(?,?);";
+    Connection connection = null;
+    PreparedStatement insertStmt = null;
+    try {
+      connection = connectionManager.getConnection();
+      insertStmt = connection.prepareStatement(insertLocation);
+      insertStmt.setInt(1, location.getZipCode());
+      insertStmt.setString(2, location.getLocationName());
+      insertStmt.executeUpdate();
+      return location;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if(connection != null) {
+        connection.close();
+      }
+      if(insertStmt != null) {
+        insertStmt.close();
+      }
+    }
+  }
+
+  public Location getLocation(int ZipCode) throws SQLException {
+    String selectLocation = "SELECT LocationName,ZipCode FROM Location WHERE ZipCode=?;";
+    Connection connection = null;
+    PreparedStatement selectStmt = null;
+    ResultSet results = null;
+    try {
+      connection = connectionManager.getConnection();
+      selectStmt = connection.prepareStatement(selectLocation);
+      selectStmt.setInt(1, ZipCode);
+      results = selectStmt.executreQuery();
+      if(results.next()) {
+        String resultLocationName = results.getString("LocationName");
+        int ZipCode = results.getInt("ZipCode");
+        Location location = new Location(resultLocationName,ZipCode);
+        return location;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if(connection != null) {
+        connection.close();
+      }
+      if(selectStmt != null) {
+        selectStmt.close();
+      }
+      if(results != null) {
+        results.close();
+      }
+    }
+    return null;
+  }
+
+  public Location updatePopulation() throws SQLException {
 
   }
 
-  public Location getLocation() throws SQLException {
+  public Location updateLocationName() throws SQLException {
 
   }
 
-  public Location delete() throws SQLException {
+  public Location updateLocationCounty() throws SQLException {
 
+  }
+
+  public Location delete(Location location) throws SQLException {
+    String deleteLocation = "DELETE FROM Location WHERE ZipCode=?;";
+    Connection connection = null;
+    PreparedStatement deleteStmt = null;
+    try {
+      connection = connectionManager.getConnection();
+      deleteStmt = connection.prepareStatement(deleteLocation;
+      deleteStmt.setInt(1, location.getZipCode());
+      deleteStmt.executeUpdate();
+      return null;
+    } catch (SQLException e) {
+      e.printStackTrace();;
+      throw e;
+    } finally {
+      if (connection != null) {
+        connection.close();
+      }
+      if (deleteStmt != null) {
+        deleteStmt.close();
+      }
+    }
+  }
   }
 }
