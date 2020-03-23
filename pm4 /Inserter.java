527d53cd4c945e34a028b453a313d723b4d4ccbd
Amy Stefani <amystefani1@gmail.com>
2020-03-22 17:18:44 -0700

    updating airquality dao, model, and adding tests to inserter

--- pm4 /Inserter.java
+++ pm4 /Inserter.java
@@ -10,21 +10,31 @@ public class Inserter {
 
   public static void main(String[] args) throws SQLException {
     AirQualityDAO airQualityDAO = AirQualityDAO.getInstance();
-    CountyDAO countyDAO = CountyDAO.getInstance();
-    HealthCareSpendingDAO healthCareSpendingDAO = HealthCareSpendingDAO.getInstance();
-    HealthCareUtilizationDAO healthCareUtilizationDAO = HealthCareUtilizationDAO.getInstance();
-    HospitalDAO hospitalDAO = HospitalDAO.getInstance();
-    HospitalQualityDAO hospitalQualityDAO = HospitalQualityDAO.getInstance();
-    LocationDAO locationDAO = LocationDAO.getInstance();
-    StateDAO stateDAO = StateDAO.getInstance();
+//    CountyDAO countyDAO = CountyDAO.getInstance();
+//    HealthCareSpendingDAO healthCareSpendingDAO = HealthCareSpendingDAO.getInstance();
+//    HealthCareUtilizationDAO healthCareUtilizationDAO = HealthCareUtilizationDAO.getInstance();
+//    HospitalDAO hospitalDAO = HospitalDAO.getInstance();
+//    HospitalQualityDAO hospitalQualityDAO = HospitalQualityDAO.getInstance();
+//    LocationDAO locationDAO = LocationDAO.getInstance();
+//    StateDAO stateDAO = StateDAO.getInstance();
 
     //CREATE
+    AirQuality airQuality = new AirQuality(9999,"WA",2,3,4,5,6,7,8,12,43,13);
+	airQuality = airQualityDAO.create(airQuality);
 
     //READ
-
-    //UPDATE
-
+	AirQuality airQuality2 = airQualityDAO.getAirQualityFromCountyCode(9999);
+	System.out.format("Air Quality for county (code): %s in the state of %s | MaxAQI: %s, MedianAQI: %s \n",
+			airQuality2.getCountyCode(), airQuality2.getStateCode(), airQuality2.getMaxAQI(), airQuality2.getMedianAQI());
+	
+    //UPDATE	
+	airQualityDAO.updateMedianAQI(airQuality2, 678);
+	
+	AirQuality airQuality2updated = airQualityDAO.getAirQualityFromCountyCode(9999);
+	System.out.format("Air Quality for county (code): %s in the state of %s | MaxAQI: %s, MedianAQI: %s \n",
+			airQuality2updated.getCountyCode(), airQuality2updated.getStateCode(), airQuality2updated.getMaxAQI(), airQuality2updated.getMedianAQI());
+	
     //DELETE
-
+	airQualityDAO.delete(airQuality2updated);
   }
 }
