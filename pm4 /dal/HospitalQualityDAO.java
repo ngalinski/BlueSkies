e7638b057604951f0fa9f617463740831e0a6a50
Nicholas Galinski <galinski.n@husky.neu.edu>
2020-03-22 20:00:30 -0700

    updated hq dao

--- pm4 /dal/HospitalQualityDAO.java
+++ pm4 /dal/HospitalQualityDAO.java
@@ -45,16 +45,83 @@ public class HospitalQualityDAO {
     }
   }
 
-  public HospitalQuality getHQbyCode() throws SQLException {
-
+  public HospitalQuality getHQbyCode(int HospitalQualityCode) throws SQLException {
+    String selectHQ = "SELECT HospitalCode,HospitalName FROM HospitalQualityCode WHERE HospitalQualityCode=?;";
+    Connection connection = null;
+    PreparedStatement selectStmt = null;
+    ResultSet results = null;
+    try {
+      connection = connectionManager.getConnection();
+      selectStmt = connection.prepareStatement(selectHQ);
+      selectStmt.setInt(1, HospitalQualityCode);
+      results = selectStmt.executreQuery();
+      if(results.next()) {
+        String resultHospitalName = results.getString("HospitalName");
+        int HospitalCode = results.getInt("HospitalCode");
+        HospitalQuality hospitalQuality = new HospitalQuality(resultHospitalName, HospitalCode);
+        return hospitalQuality;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if(connection != null) {
+        connection.close();
+      }
+      if(selectStmt != null) {
+        selectStmt.close();
+      }
+      if(results != null) {
+        results.close();
+      }
+    }
+    return null;
   }
 
-  public HospitalQuality updateRating() throws SQLException {
-
+  public HospitalQuality updateRating(HospitalQuality hospitalQuality, int newRating) throws SQLException {
+    String updateRating = "UPDATE HospitalQuality SET Rating=?;";
+    Connection connection = null;
+    PreparedStatement updateStmt = null;
+    try {
+      connection = connectionManager.getConnection();
+      updateStmt = connection.prepareStatement(updateRating);
+      updateStmt.setString(1, newRating);
+      hospitalQuality.setRating(newRating);
+      return hospitalQuality;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if(connection != null) {
+        connection.close();
+      }
+      if(updateStmt != null) {
+        updateStmt.close();
+      }
+    }
   }
 
-  public HospitalQuality updatePatientExp() throws SQLException {
-
+  public HospitalQuality updatePatientExp(HospitalQuality hospitalQuality, int newExp) throws SQLException {
+    String updateExp = "UPDATE HospitalQuality SET PatientExperience=?;";
+    Connection connection = null;
+    PreparedStatement updateStmt = null;
+    try {
+      connection = connectionManager.getConnection();
+      updateStmt = connection.prepareStatement(updateExp);
+      updateStmt.setString(1, newExp);
+      hospitalQuality.setPatientExperience(newExp);
+      return hospitalQuality;
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if(connection != null) {
+        connection.close();
+      }
+      if(updateStmt != null) {
+        updateStmt.close();
+      }
+    }
   }
 
   public HosptialQuality delete(HospitalQuality hospitalQuality) throws SQLException {
