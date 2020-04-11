4a65afd45f56ebe45fcd1c98e1928bcd58eec52a
Nicholas Galinski <galinski.n@husky.neu.edu>
2020-04-11 11:04:51 -0700

    added imports / beginnings of DAOs

---
+++ pm4 /src/dal/SocioeconomicDAO.java
@@ -0,0 +1,24 @@
+package dal;
+
+import model.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SocioeconomicDAO {
+  protected ConnectionManager connectionManager;
+
+  // Connection
+
+  // CREATE
+
+  // READ
+
+  // UPDATE
+
+  // DELETE
+
+}
