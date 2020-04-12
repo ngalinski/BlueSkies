661acf91304e2cb6eaa245582fc4e6473474f9a1
Amy Stefani <amystefani1@gmail.com>
2020-04-12 01:05:01 -0700

    socioeconomic dao completed

--- pm4 /src/dal/SocioeconomicDAO.java
+++ pm4 /src/dal/SocioeconomicDAO.java
@@ -67,98 +67,161 @@ public class SocioeconomicDAO {
       }
       
     }
-//  	// READ 
-//  	public HospitalQuality getHospitalQualityFromHospitalCode(int hospitalCode) throws SQLException {
-//  		String selectHospitalQualityCode = "SELECT HospitalQualityCode,HospitalCode,OverallRating,Mortality,Safety,Readmission,PatientExperience,Effectiveness,Timeliness,EfficientUseMedicalImaging FROM HospitalQuality WHERE HospitalCode=?;";
-//  		Connection connection = null;
-//  		PreparedStatement selectStmt = null;
-//  		ResultSet results = null;
-//  		try {
-//  			connection = connectionManager.getConnection();
-//  			selectStmt = connection.prepareStatement(selectHospitalQualityCode);
-//  			selectStmt.setInt(1, hospitalCode);
-//
-//  			results = selectStmt.executeQuery();
-//
-//  			if(results.next()) {
-//  				int hospitalQualityCode = results.getInt("HospitalQualityCode");
-//  				int resultHospitalCode = results.getInt("HospitalCode");
-//  				int rating = results.getInt("OverallRating");
-//  				int mortality = results.getInt("Mortality");
-//  				int safety = results.getInt("Safety");
-//  				int readmission = results.getInt("Readmission");
-//  				int patientExperience = results.getInt("PatientExperience");
-//  				int effectiveness = results.getInt("Effectiveness");
-//  				int timeliness = results.getInt("Timeliness");
-//  				int efficientUseMedicalImaging = results.getInt("EfficientUseMedicalImaging");
-//
-//  				HospitalQuality hospitalQuality = new HospitalQuality(hospitalQualityCode, resultHospitalCode, rating, mortality, safety, readmission, patientExperience, effectiveness, timeliness, efficientUseMedicalImaging);
-//  				return hospitalQuality;
-//  			}
-//  		} catch (SQLException e) {
-//  			e.printStackTrace();
-//  			throw e;
-//  		} finally {
-//  			if(connection != null) {
-//  				connection.close();
-//  			}
-//  			if(selectStmt != null) {
-//  				selectStmt.close();
-//  			}
-//  			if(results != null) {
-//  				results.close();
-//  			}
-//  		}
-//  		return null;
-//  	}
-//
-//
-//    public HospitalQuality updateOverallRating(HospitalQuality hospitalQuality, int overallRating) throws SQLException {
-//      String updateOverallRating = "UPDATE HospitalQuality SET OverallRating=? WHERE HospitalQualityCode=?;";
-//      Connection connection = null;
-//      PreparedStatement updateStmt = null;
-//      try {
-//        connection = connectionManager.getConnection();
-//        updateStmt = connection.prepareStatement(updateOverallRating);
-//        updateStmt.setInt(1, overallRating);
-//        updateStmt.setInt(2, hospitalQuality.getHospitalQualityCode());
-//  	  updateStmt.executeUpdate();
-//
-//        hospitalQuality.setOverallRating(overallRating);
-//        return hospitalQuality;
-//      } catch (SQLException e) {
-//        e.printStackTrace();
-//        throw e;
-//      } finally {
-//        if(connection != null) {
-//          connection.close();
-//        }
-//        if(updateStmt != null) {
-//          updateStmt.close();
-//        }
-//      }
-//    }
-//
-//    public HospitalQuality delete(HospitalQuality hospitalQuality) throws SQLException {
-//      String deleteHospitalQuality = "DELETE FROM HospitalQuality WHERE HospitalQualityCode=?;";
-//      Connection connection = null;
-//      PreparedStatement deleteStmt = null;
-//      try {
-//        connection = connectionManager.getConnection();
-//        deleteStmt = connection.prepareStatement(deleteHospitalQuality);
-//        deleteStmt.setInt(1, hospitalQuality.getHospitalQualityCode());
-//        deleteStmt.executeUpdate();
-//        return null;
-//      } catch (SQLException e) {
-//        e.printStackTrace();;
-//        throw e;
-//      } finally {
-//        if (connection != null) {
-//          connection.close();
-//        }
-//        if (deleteStmt != null) {
-//          deleteStmt.close();
-//        }
-//      }
-//    }
+  	// READ From CountyCode
+  	public Socioeconomic getSocioeconomicFromCountyCode(int countyCode) throws SQLException {
+  		String selectSocioeconomicFromCountyCode = "SELECT SocioeconomicCode,CountyCode,UnemploymentRate,PercentPopulationInPoverty,MedianHouseholdIncome,PercentLessThanHSDiploma,PercentHSDiplomaOnly,PercentSomeCollegeOnly,PercentBachelorsOrMore FROM HospitalQuality WHERE CountyCode=?;";
+  		Connection connection = null;
+  		PreparedStatement selectStmt = null;
+  		ResultSet results = null;
+  		try {
+  			connection = connectionManager.getConnection();
+  			selectStmt = connection.prepareStatement(selectSocioeconomicFromCountyCode);
+  			selectStmt.setInt(1, countyCode);
+
+  			results = selectStmt.executeQuery();
+
+  			if(results.next()) {
+  				int socioeconomicCode = results.getInt("SocioeconomicCode");
+  				int resultCountyCode = results.getInt("CountyCode");
+  				int unemploymentRate = results.getInt("UnemploymentRate");
+  				int percentPopulationInPoverty = results.getInt("PercentPopulationInPoverty");
+  				int medianHouseholdIncome = results.getInt("MedianHouseholdIncome");
+  				int percentLessThanHSDiploma = results.getInt("PercentLessThanHSDiploma");
+  				int percentHSDiplomaOnly = results.getInt("PercentHSDiplomaOnly");
+  				int percentSomeCollegeOnly = results.getInt("PercentSomeCollegeOnly");
+  				int percentBachelorsOrMore = results.getInt("PercentBachelorsOrMore");
+
+  				Socioeconomic socioeconomic = new Socioeconomic(socioeconomicCode, resultCountyCode, unemploymentRate, percentPopulationInPoverty, medianHouseholdIncome, percentLessThanHSDiploma, percentHSDiplomaOnly, percentSomeCollegeOnly, percentBachelorsOrMore);
+  				return socioeconomic;
+  			}
+  		} catch (SQLException e) {
+  			e.printStackTrace();
+  			throw e;
+  		} finally {
+  			if(connection != null) {
+  				connection.close();
+  			}
+  			if(selectStmt != null) {
+  				selectStmt.close();
+  			}
+  			if(results != null) {
+  				results.close();
+  			}
+  		}
+  		return null;
+  	}
+  	
+ // READ From SocioeconomicCode
+   	public Socioeconomic getSocioeconomicFromSocioeconomicCode(int socioeconomicCode) throws SQLException {
+   		String  selectSocioeconomicFromSocioeconomicCode = "SELECT SocioeconomicCode,CountyCode,UnemploymentRate,PercentPopulationInPoverty,MedianHouseholdIncome,PercentLessThanHSDiploma,PercentHSDiplomaOnly,PercentSomeCollegeOnly,PercentBachelorsOrMore FROM HospitalQuality WHERE SocioeconomicCode=?;";
+   		Connection connection = null;
+   		PreparedStatement selectStmt = null;
+   		ResultSet results = null;
+   		try {
+   			connection = connectionManager.getConnection();
+   			selectStmt = connection.prepareStatement(selectSocioeconomicFromSocioeconomicCode);
+   			selectStmt.setInt(1, socioeconomicCode);
+
+   			results = selectStmt.executeQuery();
+
+   			if(results.next()) {
+   				int resultSocioeconomicCode = results.getInt("SocioeconomicCode");
+   				int countyCode = results.getInt("CountyCode");
+   				int unemploymentRate = results.getInt("UnemploymentRate");
+   				int percentPopulationInPoverty = results.getInt("PercentPopulationInPoverty");
+   				int medianHouseholdIncome = results.getInt("MedianHouseholdIncome");
+   				int percentLessThanHSDiploma = results.getInt("PercentLessThanHSDiploma");
+   				int percentHSDiplomaOnly = results.getInt("PercentHSDiplomaOnly");
+   				int percentSomeCollegeOnly = results.getInt("PercentSomeCollegeOnly");
+   				int percentBachelorsOrMore = results.getInt("PercentBachelorsOrMore");
+
+   				Socioeconomic socioeconomic = new Socioeconomic(resultSocioeconomicCode, countyCode, unemploymentRate, percentPopulationInPoverty, medianHouseholdIncome, percentLessThanHSDiploma, percentHSDiplomaOnly, percentSomeCollegeOnly, percentBachelorsOrMore);
+   				return socioeconomic;
+   			}
+   		} catch (SQLException e) {
+   			e.printStackTrace();
+   			throw e;
+   		} finally {
+   			if(connection != null) {
+   				connection.close();
+   			}
+   			if(selectStmt != null) {
+   				selectStmt.close();
+   			}
+   			if(results != null) {
+   				results.close();
+   			}
+   		}
+   		return null;
+   	}
+
+
+    public Socioeconomic updateSocioeconomic(Socioeconomic socioeconomic, Socioeconomic newSocioeconomic) throws SQLException {
+      String updateSocioeconomicAll = "UPDATE Socioeconomic SET CountyCode=?,UnemploymentRate=?,PercentPopulationInPoverty=?, "
+      		+ "MedianHouseholdIncome=?, PercentLessThanHSDiploma=?,PercentHSDiplomaOnly=?,PercentSomeCollegeOnly=?,"
+      		+ "PercentBachelorsOrMore=? WHERE SocioeconomicCode=?;";
+      
+      Connection connection = null;
+      PreparedStatement updateStmt = null;
+      try {
+        connection = connectionManager.getConnection();
+        updateStmt = connection.prepareStatement(updateSocioeconomicAll);
+        updateStmt.setInt(1, socioeconomic.getSocioeconomicCode());
+        updateStmt.setInt(2, socioeconomic.getCountyCode());
+        updateStmt.setDouble(3, socioeconomic.getUnemploymentRate());
+        updateStmt.setDouble(4, socioeconomic.getPercentPopulationInPoverty());
+        updateStmt.setInt(5, socioeconomic.getMedianHouseholdIncome());
+	  	updateStmt.setDouble(6, socioeconomic.getPercentLessThanHSDiploma());
+	  	updateStmt.setDouble(7, socioeconomic.getPercentHSDiplomaOnly());
+	  	updateStmt.setDouble(8, socioeconomic.getPercentSomeCollegeOnly());
+	  	updateStmt.setDouble(9, socioeconomic.getPercentBachelorsOrMore());
+		updateStmt.executeUpdate();
+
+	  	
+		socioeconomic.setSocioeconomicCode(newSocioeconomic.getCountyCode());
+		socioeconomic.setCountyCode(newSocioeconomic.getCountyCode());
+		socioeconomic.setUnemploymentRate(newSocioeconomic.getUnemploymentRate());
+		socioeconomic.setPercentPopulationInPoverty(newSocioeconomic.getPercentPopulationInPoverty());
+		socioeconomic.setMedianHouseholdIncome(newSocioeconomic.getMedianHouseholdIncome());
+		socioeconomic.setPercentLessThanHSDiploma(newSocioeconomic.getPercentLessThanHSDiploma());
+		socioeconomic.setPercentHSDiplomaOnly(newSocioeconomic.getPercentHSDiplomaOnly());
+		socioeconomic.setPercentSomeCollegeOnly(newSocioeconomic.getPercentSomeCollegeOnly());
+		socioeconomic.setPercentBachelorsOrMore(newSocioeconomic.getPercentBachelorsOrMore());
+
+        return socioeconomic;
+      } catch (SQLException e) {
+        e.printStackTrace();
+        throw e;
+      } finally {
+        if(connection != null) {
+          connection.close();
+        }
+        if(updateStmt != null) {
+          updateStmt.close();
+        }
+      }
+    }
+
+    public Socioeconomic delete(Socioeconomic socioeconomic) throws SQLException {
+      String deleteSocioeconomic = "DELETE FROM Socioeconomic WHERE SocioeconomicCode=?;";
+      Connection connection = null;
+      PreparedStatement deleteStmt = null;
+      try {
+        connection = connectionManager.getConnection();
+        deleteStmt = connection.prepareStatement(deleteSocioeconomic);
+        deleteStmt.setInt(1, socioeconomic.getSocioeconomicCode());
+        deleteStmt.executeUpdate();
+        return null;
+      } catch (SQLException e) {
+        e.printStackTrace();;
+        throw e;
+      } finally {
+        if (connection != null) {
+          connection.close();
+        }
+        if (deleteStmt != null) {
+          deleteStmt.close();
+        }
+      }
+    }
   }
